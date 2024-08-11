6a194d438912c91e95a631e355a81f11ef5732bb
Emirhan Kartal <emirhan.kartal@twiser.com>
2024-08-11 04:01:11 +0300

    requirements.txt update needed

--- fetchLoLData.py
+++ fetchLoLData.py
@@ -3,15 +3,22 @@ import numpy as np
 import json
 import urllib.parse
 from collections import Counter
+import os
+from dotenv import load_dotenv
+
+
 
 
 async def get_lol_info(summoner_name, summoner_tag):
+
+    load_dotenv()
+    TOKEN = os.getenv('RIOT_API_KEY')
     
     
     url_summoner_name = urllib.parse.quote(summoner_name)
     url_summoner_tag = urllib.parse.quote(summoner_tag)
     
-    account_url = f'https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{url_summoner_name}/{url_summoner_tag}?api_key=RGAPI-4a128b28-4e48-4d8a-9efd-7edc13579d3f'
+    account_url = f'https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{url_summoner_name}/{url_summoner_tag}?api_key={TOKEN}'
 
     response = requests.get(account_url)
     user_body = response.json()
@@ -19,7 +26,7 @@ async def get_lol_info(summoner_name, summoner_tag):
     user_puuid = user_body['puuid']
     matchHistoryLength = 20
 
-    url = f'https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{user_puuid}/ids?start=0&count={matchHistoryLength}&api_key=RGAPI-4a128b28-4e48-4d8a-9efd-7edc13579d3f'
+    url = f'https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{user_puuid}/ids?start=0&count={matchHistoryLength}&api_key={TOKEN}'
 
     response = requests.get(url)
 
@@ -29,7 +36,7 @@ async def get_lol_info(summoner_name, summoner_tag):
 
 
     for match in matchIDList:
-        match_url = f'https://europe.api.riotgames.com/lol/match/v5/matches/{match}?api_key=RGAPI-4a128b28-4e48-4d8a-9efd-7edc13579d3f'
+        match_url = f'https://europe.api.riotgames.com/lol/match/v5/matches/{match}?api_key={TOKEN}'
         response = requests.get(match_url)
         response = response.json()
         player_list = response['info']['participants']
